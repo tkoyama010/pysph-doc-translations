9ff6d0adfefa2a6016ca4d710f044f8f1e7d50b6
Tetsuo Koyama <tkoyama010@gmail.com>
2021-11-02 02:38:25 +0900

    📝 Build RTD (#2)
    * 📝 Build RTD
    
    * Update requirements.txt
    
    * Update main.yml
    
    * Update requirements.txt
    
    * Update requirements.txt
    
    * Create .readthedocs.yaml
    
    * Create environment.yml
    
    * Update environment.yml
    
    * Update .readthedocs.yaml
    
    * Update .gitmodules
    
    * Add pysph
    
    * Update conf.py
    
    * Update pysph
    
    * Update conf.py
    
    * Update conf.py
    
    * Update conf.py
    
    * Update conf.py
    
    * Update main.yml
    
    * Update conf.py
    
    * Update conf.py
    
    * Update requirements.txt
    
    * Update requirements.txt

---
+++ conf.py
@@ -0,0 +1,29 @@
+# basedir is set by <lang>/conf.py
+"""
+Use "-D language=<LANG>" option to build a localized CoFEA document.
+For example::
+
+    sphinx-build -D language=ja -b html . _build/html
+
+This conf.py do:
+
+- Specify `locale_dirs` and `gettext_compact`.
+- Overrides source directory as 'pysph/docs/source`.
+
+"""
+import os
+
+from sphinx.util.pycompat import execfile_
+
+basedir = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "pysph/docs/source"
+)
+
+execfile_(os.path.join(basedir, "conf.py"), globals())
+
+locale_dirs = [os.path.join(basedir, "../../../locale/")]
+
+
+def setup(app):
+    app.srcdir = basedir
+    app.confdir = app.srcdir
