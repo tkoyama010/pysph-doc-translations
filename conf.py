81ea158ab0b6848df01301d8ba5737679044274b
Tetsuo Koyama <tkoyama010@gmail.com>
2021-11-02 02:12:14 +0900

    Update conf.py

--- conf.py
+++ conf.py
@@ -15,9 +15,11 @@ import os
 
 from sphinx.util.pycompat import execfile_
 
-basedir = os.path.dirname(os.path.abspath(__file__))
+basedir = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "pysph/docs/source"
+)
 
-execfile_(os.path.join(basedir, "pysph/docs/source/conf.py"), globals())
+execfile_(os.path.join(basedir, "conf.py"), globals())
 
 locale_dirs = [os.path.join(basedir, "locale/")]
 
